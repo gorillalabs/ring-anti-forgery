58e4f28382691fe200d2a8c7536002e88ac98230
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-02 11:40:34 +0100

    fix project setup
    not in monolith setup normally

--- project.clj
+++ project.clj
@@ -3,13 +3,11 @@
   :url "https://github.com/ring-clojure/ring-anti-forgery"
   :license {:name "The MIT License"
             :url  "http://opensource.org/licenses/MIT"}
-  :monolith/inherit true
   :middleware [leiningen.v/dependency-version-from-scm
                leiningen.v/version-from-scm
                leiningen.v/add-workspace-data]
 
-  :plugins [[lein-monolith "1.0.1"]
-            [com.roomkey/lein-v "6.2.0"]]
+  :plugins [[com.roomkey/lein-v "6.2.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [crypto-random "1.2.0"]
                  [crypto-equality "1.0.0"]
@@ -22,4 +20,4 @@
   {:dev {:dependencies [[ring/ring-mock "0.3.0"]]}
    :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
    :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha11"]]}})
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}})
