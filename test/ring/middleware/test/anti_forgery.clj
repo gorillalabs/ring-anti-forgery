9dbfd848c344ffbda0bda67cd650f7ac753c12c9
Dr. Christian Betz <cb@cypp.hamburg>
2018-01-19 12:52:34 +0100

    remove encryption based strategies
    encryption based strategies moved to a separate project, to keep
    backwards compatibility for all the users of ring-anti-forgery.

--- test/ring/middleware/test/anti_forgery.clj
+++ test/ring/middleware/test/anti_forgery.clj
@@ -1,70 +1,10 @@
 (ns ring.middleware.test.anti-forgery
   (:require [ring.middleware.anti-forgery :as af]
-            [ring.middleware.anti-forgery.strategy.encrypted-token :as encrypted-token]
-            [ring.middleware.anti-forgery.strategy.signed-token :as signed-token]
-            [buddy.core.keys :as keys]
-            [clj-time.core :as time]
             [ring.middleware.anti-forgery.strategy :as strategy]
             [ring.middleware.anti-forgery.strategy.session :as session])
   (:use clojure.test
         ring.middleware.anti-forgery
         ring.mock.request))
-
-(def ^:private expires-in-one-hour (time/hours 1))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Things necessary to test signed-token-strategy
-;;
-
-(def ^:private pubkey (keys/public-key "dev-resources/test-certs/pubkey.pem"))
-(def ^:private privkey (keys/private-key "dev-resources/test-certs/privkey.pem" "antiforgery"))
-(def ^:private other-private-key (keys/private-key "dev-resources/test-certs/privkey-other.pem" "other"))
-
-(def ^:private signed-token-sms (signed-token/->SignedTokenSMS pubkey privkey expires-in-one-hour :identity))
-
-(def ^:private signed-token-options {:state-management-strategy signed-token-sms})
-
-
-(defn create-signed-csrf-token
-  ([privkey expiration]
-   (strategy/find-or-create-token (signed-token/->SignedTokenSMS nil privkey expiration :identity) nil))
-  ([privkey expiration subject]
-   (strategy/find-or-create-token (signed-token/->SignedTokenSMS nil privkey expiration :identity) {:identity subject})))
-
-(defn- valid-signed-token? [public-key token]
-  (strategy/valid-token?
-    (signed-token/->SignedTokenSMS public-key nil nil :identity)
-    token
-    identity))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; Things necessary to test encrypted-token-strategy
-;;
-
-(def ^:private secret "secret-to-validate-token-after-decryption-to-make-sure-i-encrypted-stuff")
-
-(def ^:private encrypted-token-sms (encrypted-token/->EncryptedTokenSMS
-                                     (encrypted-token/sha256 secret)
-                                     expires-in-one-hour :identity))
-
-(def ^:private encrypted-token-options {:state-management-strategy encrypted-token-sms})
-
-(defn create-encrypted-csrf-token
-  ([secret expiration]
-   (strategy/find-or-create-token (encrypted-token/->EncryptedTokenSMS (encrypted-token/sha256 secret) expiration :identity) nil))
-  ([secret expiration subject]
-   (strategy/find-or-create-token (encrypted-token/->EncryptedTokenSMS (encrypted-token/sha256 secret) expiration :identity) {:identity subject})))
-
-(defn- valid-encrypted-token? [secret token]
-  (strategy/valid-token?
-    (encrypted-token/->EncryptedTokenSMS (encrypted-token/sha256 secret) nil :identity)
-    token
-    identity))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -92,112 +32,7 @@
                               (assoc :session {::af/anti-forgery-token "foo"})
                               (assoc :form-params {"__anti-forgery-token" "foo"})))))
 
-(deftest forgery-protection-via-signed-token-test
-  (let [expired-one-hour-ago (time/hours -1)
 
-        response {:status 200, :headers {}, :body "Foo"}
-        handler (wrap-anti-forgery (constantly response) signed-token-options)
-        status= (partial status=* handler)]
-
-    (testing "without anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/"))
-                        403 (-> (request :post "/")
-                                (assoc :identity "user-id"))))
-
-    (testing "with ill-formated anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                ;; anti-forgery-token not decryptable with our key
-                                (assoc :form-params {"__anti-forgery-token" "bar"}))))
-
-    (testing "with non-decryptable anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                ;; anti-forgery-token not decryptable with our key
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token other-private-key expired-one-hour-ago)}))))
-    (testing "with expired anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expired-one-hour-ago)}))))
-    (testing "with anti-forgery-token for wrong subject"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expires-in-one-hour "user-id")})
-                                (assoc :identity "another-user-id"))))
-    (testing "with anti-forgery-token for no subject"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expires-in-one-hour)})
-                                (assoc :identity "user-id"))))
-
-    (testing "with correct anti-forgery-token if no subject is given.
-    (Attention: Has different nounce, but that's ok)"
-      (are [status req] (status= status req)
-                        200 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expires-in-one-hour)}))))
-
-    (testing "with correct anti-forgery-token if subject is given.
-    (Attention: Has different nounce, but that's ok)"
-      (are [status req] (status= status req)
-                        200 (-> (request :post "/")
-                                (assoc :identity "user-id")
-                                (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expires-in-one-hour "user-id")}))))
-    ))
-
-
-(deftest forgery-protection-via-encrypted-token-test
-  (let [expired-one-hour-ago (time/hours -1)
-
-        response {:status 200, :headers {}, :body "Foo"}
-        handler (wrap-anti-forgery (constantly response) encrypted-token-options)
-        status= (partial status=* handler)]
-
-    (testing "without anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/"))
-                        403 (-> (request :post "/")
-                                (assoc :identity "user-id"))))
-
-    (testing "with ill-formated anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" "bar"}))))
-
-    (testing "with non-decryptable anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                ;; anti-forgery-token not decryptable with our key
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token "another-secret" expired-one-hour-ago)}))))
-
-    (testing "with expired anti-forgery-token"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token secret expired-one-hour-ago)}))))
-    (testing "with anti-forgery-token for wrong subject"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token secret expires-in-one-hour "user-id")})
-                                (assoc :identity "another-user-id"))))
-    (testing "with anti-forgery-token for no subject"
-      (are [status req] (status= status req)
-                        403 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token secret expires-in-one-hour)})
-                                (assoc :identity "user-id"))))
-
-    (testing "with correct anti-forgery-token if no subject is given.
-    (Attention: Has different nounce, but that's ok)"
-      (are [status req] (status= status req)
-                        200 (-> (request :post "/")
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token secret expires-in-one-hour)}))))
-
-    (testing "with correct anti-forgery-token if subject is given.
-    (Attention: Has different nounce, but that's ok)"
-      (are [status req] (status= status req)
-                        200 (-> (request :post "/")
-                                (assoc :identity "user-id")
-                                (assoc :form-params {"__anti-forgery-token" (create-encrypted-csrf-token secret expires-in-one-hour "user-id")}))))
-    ))
 
 (deftest request-method-test
   (let [response {:status 200, :headers {}, :body "Foo"}
@@ -211,29 +46,7 @@
                       403 (request :patch "/")
                       403 (request :delete "/"))))
 
-(deftest request-method-via-signed-token-test
-  (let [response {:status 200, :headers {}, :body "Foo"}
-        handler (wrap-anti-forgery (constantly response) signed-token-options)]
-    (are [status req] (= (:status (handler req)) status)
-                      200 (request :head "/")
-                      200 (request :get "/")
-                      200 (request :options "/")
-                      403 (request :post "/")
-                      403 (request :put "/")
-                      403 (request :patch "/")
-                      403 (request :delete "/"))))
 
-(deftest request-method-via-encrypted-token-test
-  (let [response {:status 200, :headers {}, :body "Foo"}
-        handler (wrap-anti-forgery (constantly response) encrypted-token-options)]
-    (are [status req] (= (:status (handler req)) status)
-                      200 (request :head "/")
-                      200 (request :get "/")
-                      200 (request :options "/")
-                      403 (request :post "/")
-                      403 (request :put "/")
-                      403 (request :patch "/")
-                      403 (request :delete "/"))))
 
 (deftest csrf-header-test
   (let [response {:status 200, :headers {}, :body "Foo"}
@@ -273,34 +86,6 @@
       (is (= (get-in response [:session ::af/anti-forgery-token])
              (:body response))))))
 
-
-(deftest new-token-via-signed-token-test
-  (letfn [(handler [request]
-            {:status  200
-             :headers {}
-             :body    (new-token signed-token-sms request)})]
-    (let [response ((wrap-anti-forgery handler signed-token-options) (request :get "/"))]
-      (is (valid-signed-token? pubkey (:body response))))))
-
-
-(deftest token-binding-via-signed-token-test
-  (letfn [(handler [request]
-            {:status  200
-             :headers {}
-             :body    @*anti-forgery-token*})]
-    (let [response ((wrap-anti-forgery handler signed-token-options) (request :get "/"))]
-      (is (valid-signed-token? pubkey (:body response))))))
-
-
-(deftest token-binding-via-encrypted-token-test
-  (letfn [(handler [request]
-            {:status  200
-             :headers {}
-             :body    @*anti-forgery-token*})]
-    (let [response ((wrap-anti-forgery handler encrypted-token-options) (request :get "/"))]
-      (is (< (count (:body response)) 3000))
-      (is (valid-encrypted-token? secret (:body response))))))
-
 (deftest nil-response-test
   (letfn [(handler [request] nil)]
     (let [response ((wrap-anti-forgery handler) (request :get "/"))]
@@ -338,12 +123,6 @@
     (is (contains? session ::af/anti-forgery-token))
     (is (= (session "foo") "bar"))))
 
-(deftest no-session-response-via-signed-token-test
-  (let [response {:status 200 :headers {} :session {"foo" "bar"} :body nil}
-        handler (wrap-anti-forgery (constantly response) signed-token-options)
-        session (:session (handler (request :get "/")))]
-    (is (not (contains? session ::af/anti-forgery-token)))
-    (is (= (session "foo") "bar"))))
 
 (deftest custom-error-response-test
   (let [response {:status 200, :headers {}, :body "Foo"}
@@ -415,26 +194,3 @@
         (is (not (realized? ex)))
         (is (= (:status @resp) 200))))))
 
-
-
-(deftest forgery-protection-cps-via-signed-token-test
-  (let [response {:status 200, :headers {}, :body "Foo"}
-        handler (wrap-anti-forgery (fn [_ respond _] (respond response)) signed-token-options)]
-
-    (testing "missing token"
-      (let [req (-> (request :post "/")
-                    (assoc :form-params {"__anti-forgery-token" "foo"}))
-            resp (promise)
-            ex (promise)]
-        (handler req resp ex)
-        (is (not (realized? ex)))
-        (is (= (:status @resp) 403))))
-
-    (testing "valid token"
-      (let [req (-> (request :post "/")
-                    (assoc :form-params {"__anti-forgery-token" (create-signed-csrf-token privkey expires-in-one-hour)}))
-            resp (promise)
-            ex (promise)]
-        (handler req resp ex)
-        (is (not (realized? ex)))
-        (is (= (:status @resp) 200))))))
